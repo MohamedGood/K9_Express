111886c7a9317da70ea907d974786c9bdaf1dc7a
Mohamed Goodwin <mohamed.goodwin@gmail.com>
2015-12-03 17:11:26 -0500

    update seed data and schema file

---
+++ db/schema.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/k9");
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var k9Schema = new Schema(
+  {
+    name: String,
+    breed: String,
+    photoUrl: String,
+    location: [{type: ObjectId, ref: "Location"}]
+  },
+  {
+    toObject: {virtuals: true},
+    toJSON: {virtuals: true}
+  }
+);
+
+k9Schema.virtual("id").get(function(){
+  return this._id;
+});
+
+var LocationSchema = new Schema({
+  city: String,
+  state: String,
+  zipcode: String,
+  address: {type: ObjectId, ref: "k9"}
+});
+
+var k9Model = mongoose.model("k9", ArtistSchema);
+var LocationModel = mongoose.model("Location", LocationSchema);
--- db/seeds.js
+++ db/seeds.js
@@ -1,1 +1,7 @@
 var k9Database = require("./connection");
+
+// Require the connection to data files.
+var data = {
+  markets: require("./k9"),
+  vendors: require("./location"),
+};
