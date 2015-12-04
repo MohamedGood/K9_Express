9fc5caff44e41f42e2da3272864dfec2fbd2824a
Mohamed Goodwin <mohamed.goodwin@gmail.com>
2015-12-04 15:23:21 -0500

    added locations.js to controllers and modified index.html file

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require('mongoose')
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.set("view engine", "hbs");
+
+app.get("/", function(req, res){
+ res.render("app.js");
+});
+
+app.use("/k9", require("./controllers/adopt_k9s.js"));
+app.use("/location", require("./controllers/locations.js"));
+
 
 app.listen(3000, function(){
   console.log("Wheee! I'm working!");
