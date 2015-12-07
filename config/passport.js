475d2f7755aecb075b4101b0372fd4c9b38b4e5a
Mohamed Goodwin <mohamed.goodwin@gmail.com>
2015-12-07 17:29:20 -0500

    changes to config files

--- config/passport.js
+++ config/passport.js
@@ -2,69 +2,27 @@ var LocalStrategy   = require('passport-local').Strategy;
 var User            = require('../models/user');
 
 module.exports = function(passport) {
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser(function(id, callback) {
-    User.findById(id, function(err, user) {
-      callback(err, user);
-    });
-  });
-
-  passport.use('local-signup', new LocalStrategy({
+  passport.use('local-signup', new LocalStrategy{(
     usernameField : 'email',
-    passwordField : 'password',
+    passowrdField : 'password',
     passReqToCallback : true
   }, function(req, email, password, callback) {
-    // Find a user with this e-mail
-    User.findOne({ 'local.email' :  email }, function(err, user) {
+    User.findONe({ 'local.email' : email }, fucntion(err, user) {
       if (err) return callback(err);
-      // If there already is a user with this email display the following message
-      if (user) {
-        return callback(null, false, req.flash('signupMessage', 'This email is already used.'));
+    }
+if (user) {
+  return callback(null, false, req.flash('signupMessage', 'This email is already used.'));
 
-      } if (user) {
-        return callback(true, req.flash('signupMessage', 'You have successfuly logged in.')); // this is not working... I need to figure out how to display a message when the user is authenticated.
+} else {
 
-      } else {
-        // There is no email registered with this email
+    var NewUser        = new User();
+    NewUser.local.email = email;
+    newUser.local.password = newUser.encrypt(passowrd):
 
-        // Create a new user
-        var newUser            = new User();
-        newUser.local.email    = email;
-        newUser.local.password = newUser.encrypt(password);
-
-        newUser.save(function(err) {
-          if (err) throw err;
-          return callback(null, newUser);
-        });
-      }
+    newUser.save(function(err) {
+      if (err)throw err;
+      return callback(null, newUser);
     });
-  }));
-
-  passport.use('local-login', new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-  }, function(req, email, password, callback) {
-
-    // Search for a user with this email
-    User.findOne({ 'local.email' :  email }, function(err, user) {
-      if (err) {
-        return callback(err);
-      }
-
-      // If no user is found
-      if (!user) {
-        return callback(null, false, req.flash('loginMessage', 'No user found.'));
-      }
-      // Wrong password
-      if (!user.validPassword(password)) {
-        return callback(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-      }
-
-      return callback(null, user);
-    });
-  }));
-};
+     }
+  });
+}));
